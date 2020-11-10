d05107b0ad31554f62f755efd1d5becd59045514
shamaz332 <shamazsaeed332@gmail.com>
2020-11-10 22:47:57 +0500

    data fetching from faunadb successful

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: '/.netlify/functions/graphql_hello',
+    uri: '/.netlify/functions/graphql_faunadb',
     fetch,
   }),
   cache: new InMemoryCache()
